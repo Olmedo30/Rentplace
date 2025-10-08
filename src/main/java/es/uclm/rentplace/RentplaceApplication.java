fe0032e794d0cc3430e8221640fea33dc82e6799
Olmedo30 <dargo305.yt@gmail.com>
2025-10-08 13:08:39 +0200

    Commit inicial de Rentplace

---
+++ src/main/java/es/uclm/rentplace/RentplaceApplication.java
@@ -0,0 +1,13 @@
+package es.uclm.rentplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/uclm/rentplace/RentplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uclm.rentplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
