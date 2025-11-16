50a82ee519f11d69164057ee6505e4f6c5755aa0
PabloGGplayer <tordesilla8@gmail.com>
2025-11-16 20:58:24 +0100

    Definir atributos principales de Propiedad

--- src/main/java/es/uclm/rentplace/entity/Propiedad.java
+++ src/main/java/es/uclm/rentplace/entity/Propiedad.java
@@ -1,7 +1,47 @@
 package es.uclm.rentplace.entity;
 
 import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
+@Entity
 public class Propiedad {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "propiedades_id")
+	private Long id;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "propietario_id", nullable = false)
+    private Usuario propietario;
+	
+	@Column(name = "titulo", length = 150, nullable = false)
+    private String titulo;
+	
+	@Lob
+	@Column(name = "descripcion", columnDefinition = "TEXT")
+	private String descripcion;
+	
+	@Column(name = "direccion", length = 200)
+	private String direccion;
+	
+	@Column(name = "ciudad", length = 80)
+    private String ciudad;
+	
+
+    @Column(name = "tipo_inmueble", length = 50)
+    private String tipoInmueble;
+
+    @Column(name = "habitaciones")
+    private Integer habitaciones;
+
+    @Column(name = "capacidad")
+    private Integer capacidad;
+
+    @Column(name = "precio_noche", precision = 10, scale = 2)
+    private BigDecimal precioNoche;
+	
+	
 
 }
