a46d54040a2e5b26da08433af188bcefc0a3cdc6
PabloGGplayer <tordesilla8@gmail.com>
2025-11-17 09:44:40 +0100

    Validaciones básicas al registrar propiedad

--- src/main/java/es/uclm/rentplace/service/PropiedadService.java
+++ src/main/java/es/uclm/rentplace/service/PropiedadService.java
@@ -1,5 +1,31 @@
 package es.uclm.rentplace.service;
 
+import es.uclm.rentplace.entity.Propiedad;
+//import es.uclm.rentplace.entity.Propietario;
+import es.uclm.rentplace.persistence.PropiedadDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
 public class PropiedadService {
+	
+	@Autowired
+	private PropiedadDAO propiedadPersistence;
+	
+	public boolean registrarPropiedad(String titulo, String ciudad, String tipoInmueble, BigDecimal precioNoche, Usuario propietario) {
+		
+		if (propietario == null) {
+			return false;
+		}
+		if (titulo == null || titulo.isBlank()) {
+			return false; 
+		}
+		if (precioNoche == null || precioNoche.signum() < 0) {
+			return false;
+		}
+	}
 
 }
